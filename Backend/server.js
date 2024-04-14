8b79a1e5c38df370894fb8c8e5d4f740cebbd405
AI21BOT <ai21bot@gmail.com>
2024-04-14 18:40:33 +0530

    setUp

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("hii you in app");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.json(note);
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`server listening on port ${PORT}`);
+});
